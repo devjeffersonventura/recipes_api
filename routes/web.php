b3c3afefb7bcdbf0604274441002aeb7c0565289
Jefferson <jefferson@educationjourney.com>
2025-02-10 16:30:05 -0300

    remove route group

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(function () {
-    require __DIR__ . '/recipes.php';
-    require __DIR__ . '/users.php';
+Route::get('/', function () {
+    return response()->json(['message' => 'API funcionando']);
 });
