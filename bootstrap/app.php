c203c92dbca1598531c5d89df023d4781f51d313
Jefferson <jefferson@educationjourney.com>
2025-02-10 16:26:52 -0300

    add sanctum config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'v1/*',
         ]);
+
+        // Adicionando o middleware do Sanctum para APIs
+        $middleware->alias([
+            'sanctum' => EnsureFrontendRequestsAreStateful::class,
+        ]);        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
