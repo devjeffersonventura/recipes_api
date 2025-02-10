78c2a28be3644944e5500fcf108ff3ceedc09af8
Jefferson <jefferson@educationjourney.com>
2025-02-10 16:27:10 -0300

    add controller

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
+{
+    // Aqui podem ser adicionadas funcionalidades comuns para todos os controladores
+}
